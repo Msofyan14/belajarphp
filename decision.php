adac0926500bc89da1009518d6fbafd4bc294f91
Msofyan14 <mxsofyan14@gmail.com>
2022-02-10 21:57:39 +0700

    Create decision.php

---
+++ decision.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Decision</title>
+</head>
+<body>
+    <h1>Logika di PHP</h1>
+    <?php 
+    
+    $password = '1234';
+    $password2 = 1234;
+    if($password === $password2 ) {
+        echo 'anda berhasil masuk !';
+    } else {
+        echo ' gagal ! password anda salah !';
+    }
+    
+    
+    
+    ?>
+</body>
+</html>
