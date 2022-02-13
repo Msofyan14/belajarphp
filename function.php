dc293bb91bf4dff97f18429451d06215867bd09d
Msofyan14 <mxsofyan14@gmail.com>
2022-02-13 08:32:19 +0700

    add function

--- function.php
+++ function.php
@@ -8,16 +8,45 @@
 </head>
 <body>
     <?php 
-    function print_text($text, $angka) {
-        $text = 'gedung ' . $text . " ". $angka;
-        echo "------";
-        echo  $text;
-        echo "------" . "<br>";
-    }
-    print_text("koding" , 1);
-    print_text("juang", 2);
-    print_text("Sate", 3);
+    // function print_text($text, $angka) {
+    //     $text = 'gedung ' . $text . " ". $angka;
+    //     echo "------";
+    //     echo  $text;
+    //     echo "------" . "<br>";
+    // }
+    // print_text("koding" , 1);
+    // print_text("juang", 2);
+    // print_text("Sate", 3);
     
+    // return fungsi
+        // function menghitung($x, $y) {
+        //     $z = $x + $y;
+        //     return $z;
+        // }
+
+        // $hasil = menghitung(2,5) * 10;
+        // echo "hasil dari penjumlahan " . $hasil;
+
+    // scope
+    $a = 200;
+    $b = 300;
+
+    function menghitung() {
+        global $a, $b;
+
+        // $c = $GLOBALS['a] + $GLOBALS['b];
+        $c = $a + $b;
+        return $c;
+    }
+    // echo menghitung();
+
+    // anonymous funtion
+    $ngetik = function ($text){
+    echo $text;
+    };
+
+    $ngetik('test');
+
     ?>
 </body>
 </html>
